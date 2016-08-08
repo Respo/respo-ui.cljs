b0d5437fc54d471130fa184bc90610d37134fe07
jiyinyiyong <jiyinyiyong@gmail.com>
2016-08-08 09:30:09 +0800

    update buttom styles

--- compiled/src/respo_ui/style.cljs
+++ compiled/src/respo_ui/style.cljs
@@ -14,12 +14,14 @@
 (def card {:padding "16px"})
 
 (def button
- {:line-height "24px",
+ {:line-height 2.2,
   :min-width "64px",
   :color (hsl 0 0 100),
   :text-align "center",
   :background-color (hsl 200 80 60),
-  :cursor "pointer"})
+  :cursor "pointer",
+  :padding "0 8px",
+  :display "inline-block"})
 
 (def input
  (merge
@@ -41,13 +43,13 @@
   :flex-direction "column"})
 
 (def row
- {:align-items "strech", :display "flex", :flex-direction "row"})
+ {:align-items "stretch", :display "flex", :flex-direction "row"})
 
 (def row-center
  {:align-items "center", :display "flex", :flex-direction "row"})
 
 (def column
- {:align-items "strech", :display "flex", :flex-direction "column"})
+ {:align-items "stretch", :display "flex", :flex-direction "column"})
 
 (def column-center
  {:align-items "center", :display "flex", :flex-direction "column"})
